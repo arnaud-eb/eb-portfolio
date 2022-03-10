0b7fa18cfe5930f47a2c3fa32287b677863e1deb
arnaud-eb <arnaud.depierreux@easebest.com>
2022-03-10 18:07:33 +0100

    Add Redux DevTools enhancer as arg to createStore

--- src/store.ts
+++ src/store.ts
@@ -2,6 +2,9 @@ import { createStore } from "redux";
 
 import reducer from "./reducer";
 
-const store = createStore(reducer);
+const enhancer =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(reducer, enhancer);
 
 export default store;
